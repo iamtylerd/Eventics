c39c2bf2e97ba89f6f4233c84409b5cb2a4d1ac2
iamtylerd <tylerdanielphotography@gmail.com>
2016-10-24 10:30:51 -0500

    added a proxy

--- eventics/www/js/controllers/HomeCtrl.js
+++ eventics/www/js/controllers/HomeCtrl.js
@@ -1,7 +1,44 @@
 'use strict';
-app.controller('HomeCtrl', function($scope, $http) {
+app.controller('HomeCtrl', function($scope, $http, $rootScope, $location, $cordovaCamera) {
 
-let URL = 'http://localhost:3000'
 
+
+$rootScope.logout = function () {
+	console.log("Clicked")
+	$http
+		.post(`/api/logout`)
+		.then(function () {
+			console.log("Logout")
+			$location.url('/')
+		})
+}
+
+  document.addEventListener("deviceready", function () {
+
+    var options = {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 100,
+      targetHeight: 100,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: true,
+	  correctOrientation:true
+    };
+
+    var takePhoto = function () {
+    	console.log("camera")
+    	$cordovaCamera.getPicture(options).then(function(imageData) {
+      var image = document.getElementById('myImage');
+      image.src = "data:image/jpeg;base64," + imageData;
+    }, function(err) {
+      // error
+    });
+}
+  }, false);
 
 })
+
+
--- eventics/www/js/controllers/LoginCtrl.js
+++ eventics/www/js/controllers/LoginCtrl.js
@@ -1,8 +1,6 @@
 'use strict';
 app.controller('LoginCtrl', function($scope, $http, $location) {
 
-let URL = 'http://localhost:3000';
-
 
 $scope.loginObj = {
 	username: "",
@@ -14,30 +12,19 @@ $scope.registerObj = {
 	password: ""
 }
 
-$scope.createUser = () => {
+$scope.createUser = function () {
 	$http
-		.post(`${URL}/api/reigster`, $scope.registerObj)
-		.then((obj) => $location.url('/home'))
+		.post(`/api/reigster`, $scope.registerObj)
+		.then(function () {$location.url('/home')})
 
 }
 
-$scope.login = () => {
+$scope.login = function () {
 	$http
-// ({
-// 		method: 'POST',
-// 		url: `${URL}/api/login`,
-// 		data: $scope.loginObj,
-// 		headers: {
-// 			'Origin': `${URL}`,
-// 			'Access-Control-Request-Method': 'POST',
-// 			'Access-Control-Request-Headers': 'X-Custom-Header'
-// 		}
-// 	})
-		.post(`${URL}/api/login`, $scope.loginObj)
-		.then((obj) => {
+		.post(`/api/login`, $scope.loginObj)
+		.then(function (obj) {
 			$location.url('/home')
 			console.log(obj)
 		})
-// }
 }
 })
